01e4938d6003052d37d93de2255a98082144d944
1997alireza <torabian.alireza@gmail.com>
2018-06-30 21:01:45 +0430

    handle redirect udp client

--- client_udp.py
+++ client_udp.py
@@ -8,19 +8,38 @@ dstHost = ('127.0.0.1', 5005)
 
 
 def send_request(req):
-    client.sendto(req, dstHost)
-    resp = ""
-    print('send success')
-    while True:
-        recv = client.recv(1024)
-        if not recv:
-            break
-        resp += recv
+    try:
+        client.sendto(req, dstHost)
+        resp = ""
+        print('send success')
+        while True:
+            recv = client.recv(1024)
+            if not recv:
+                break
+            resp += recv.decode()
+    except:
+        traceback.print_exc()
     return resp
+
+
+def get_host_and_path(url):
+    url_double_slash_place = url.find('//')
+    if url_double_slash_place != -1:
+        if url[:url_double_slash_place] == 'http:' or url[:url_double_slash_place] == 'https:':
+            url = url[url_double_slash_place + len('//'):]
+    url_slash_place = url.find('/')
+    if url_slash_place == -1:
+        host = url
+        path = '/'
+    else:
+        host = url[:url_slash_place]
+        path = url[url_slash_place:]
+    return host, path
 
 
 def http_request(http_content):
     msg = send_request(http_content.encode())
+    print("msg: ", msg)
     code = int(msg.split(" ")[1])
     if code == 200:
         print('status: ok')
@@ -31,8 +50,9 @@ def http_request(http_content):
     elif code == 301 or code == 302:
         print('status: redirect')
         url = msg.split("Location: ")[1].split("\r")[0]
-        content = 'GET / HTTP/1.1\r\nHost: ' + url + '\r\n'
-        http_request(content)
+        host, path = get_host_and_path(url)
+        new_http_content = 'GET ' + path + ' HTTP/1.1\r\nHost: ' + host + '\r\n'
+        http_request(new_http_content)
     elif code == 404:
         print('status: not found')
         output_html = open("index.html", "w")
@@ -42,6 +62,7 @@ def http_request(http_content):
                           "</html>")
         output_html.close()
     else:
+        print('status: undefined code')
         output_html = open("index.html", "w")
         output_html.write("<html>"
                           "<head><meta charset=\"utf-8\"></head>"
@@ -50,9 +71,7 @@ def http_request(http_content):
         output_html.close()
 
 
-try:
-    header = '\r\n*\r\n'
-    content = 'GET / HTTP/1.1\r\nHost: aut.ac.ir\r\n'
-    http_request(content)
-except:
-    traceback.print_exc()
+header = '\r\n*\r\n'
+content = 'GET / HTTP/1.1\r\nHost: aut.ac.ir\r\n'
+http_request(content)
+
