41b9f503f3ef9b469467241b0849272cf9b062db
Alireza Heydari <alireza97hi@gmail.com>
2018-06-30 19:55:59 +0430

    proxy got dynamic on host name

--- proxy.py
+++ proxy.py
@@ -1,5 +1,21 @@
 import socket
 
+
+def find_after(s, first):
+    try:
+        start = s.index(first) + len(first)
+        return s[start:]
+    except ValueError:
+        return ""
+
+
+def find_host(data):
+    for line in data.splitlines():
+        host = find_after(line, 'Host: ')
+        host = host.strip()
+        if host != '':
+            return host
+
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 
@@ -9,24 +25,43 @@ sock.bind((UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
-    print("received message:", data)
-    sent = sock.sendto(b'hello',addr)
+    data = data.decode('utf-8')
+    
+    # convert data to string to use it
+    data = str(data)
 
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ('www.google.com', 80)
-    client_socket.connect(server_address)
+    #find_between will find and extract the host address to use it
+    host = find_host(data)
 
-    request_header = b'GET / HTTP/1.0\r\nHost: www.google.com\r\n\r\n'
-    client_socket.send(request_header)
+    # open a TCP connection to send HTTP request
+    proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # set server address and port
+    server_address = (host, 80)
+
+    # connect to server
+    proxy_socket.connect(server_address)
+
+    # sending ideal request to the connected server
+    request_header = str('GET / HTTP/1.0\r\nHost: '+host+'\r\n\r\n').encode()
+    proxy_socket.send(request_header)
 
     response = ''
     while True:
-        recv = client_socket.recv(1024)
+        # send http request to main network
+        recv = proxy_socket.recv(1024)
+
+        # end the connection if nothing is recieved
         if not recv:
             break
+        
+        # send the recieved data to the client
+        sent = sock.sendto(recv, addr)
+
+        # This is just for my debug :)))))))
         response += str(recv)
         print(str(recv))
         print('')
 
-    # print(response)
-    client_socket.close()
+
+    proxy_socket.close()
