7f3fa5b3a337e14f7f7534b546d0d8f765622147
Alireza Heydari <alireza97hi@gmail.com>
2018-06-30 20:58:57 +0430

    cache added

--- proxy.py
+++ proxy.py
@@ -1,4 +1,11 @@
 import socket
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+
+db = client.proxy_db
+
+collection = db.http_collection
 
 def find_after(s, first):
     try:
@@ -29,6 +36,10 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
 while True:
+    method_name = 'GET'
+    path = '/'
+    http_version = '1.1'
+
     data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
     data = data.decode('utf-8')
 
@@ -39,36 +50,65 @@ while True:
     host = find_host(data)
     http_describes = get_http_describes(data)
 
-    # open a TCP connection to send HTTP request
-    proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # check if exists http request in cache
+    if collection.find_one({
+        'host':host,
+        'method':method_name,
+        'path':path,
+        'http_version': http_version
+        }).count() > 0:
+        http_response = collection.find_one({
+            'host': host,
+            'method': method_name,
+            'path': path,
+            'http_version': http_version
+        })
+        for data_packet in http_response['packets']:
+            sent = sock.sendto(data_packet, addr)
+    else:
 
-    # set server address and port
-    server_address = (host, 80)
+        # open a TCP connection to send HTTP request
+        proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # connect to server
-    proxy_socket.connect(server_address)
+        # set server address and port
+        server_address = (host, 80)
 
-    # sending ideal request to the connected server
-    http_request = str(http_describes + '\r\nHost: ' + host + '\r\n\r\n')
-    request_header = http_request.encode()
-    proxy_socket.send(request_header)
+        # connect to server
+        proxy_socket.connect(server_address)
 
-    response = ''
-    while True:
-        # send http request to main network
-        recv = proxy_socket.recv(1024)
+        # sending ideal request to the connected server
+        http_request = str(http_describes + '\r\nHost: ' + host + '\r\n\r\n')
+        request_header = http_request.encode()
+        proxy_socket.send(request_header)
 
-        # end the connection if nothing is recieved
-        if not recv:
-            break
-        
-        # send the recieved data to the client
-        sent = sock.sendto(recv, addr)
+        response = ''
+        http_data = []
 
-        # This is just for my debug :)))))))
-        response += str(recv)
-        print(str(recv))
-        print('')
+        while True:
+            # send http request to main network
+            recv = proxy_socket.recv(1024)
 
+            # end the connection if nothing is recieved
+            if not recv:
+                break
+            
+            http_data.append(recv)
+            
+            # send the recieved data to the client
+            sent = sock.sendto(recv, addr)
 
-    proxy_socket.close()
+            # This is just for my debug :)))))))
+            response += str(recv)
+            print(str(recv))
+            print('')
+
+        # add data to cache to use it later
+        collection.insert_one({
+            'host': host,
+            'method': method_name,
+            'path': path,
+            'http_version': http_version,
+            'packets':http_data
+        })
+
+        proxy_socket.close()
