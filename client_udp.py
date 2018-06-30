02fb693c28a3c300762be6cc7bd062a160c59f62
Alireza Heydari <alireza97hi@gmail.com>
2018-06-30 18:21:22 +0430

    client udp got better

--- client_udp.py
+++ client_udp.py
@@ -8,7 +8,9 @@ client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 dstHost = ('127.0.0.1', 5005)
 while True:
         try:
-            client.sendto(b'GET / HTTP/1.1\r\nHost: aut.ac.ir', dstHost)
+            # header = 'asas\r\n*\r\n'
+            content = 'GET / HTTP/1.1\r\nHost: aut.ac.ir\r\n'
+            client.sendto((content.encode()), dstHost)
             print(time.time(), ' : send success')
             print(time.time()," : ",client.recv(1024))
             time.sleep(3)
