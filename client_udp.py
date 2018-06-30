6bf76255f719706e1de7c8c2150a5f9c3a152225
1997alireza <torabian.alireza@gmail.com>
2018-06-30 18:39:48 +0430

    some change

--- client_udp.py
+++ client_udp.py
@@ -7,15 +7,17 @@ import traceback
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 dstHost = ('127.0.0.1', 5005)
 # while True:
-try:
-    client.sendto(b'GET / HTTP/1.1\r\nHost: aut.ac.ir', dstHost)
-    msg = ""
-    print(time.time(), ' : send success')
-    while True:
-        recv = client.recv(1024)
-        if not recv:
-            break
-        msg += recv
+# try:
+#     client.sendto(b'GET / HTTP/1.1\r\nHost: aut.ac.ir', dstHost)
+#     msg = ""
+#     print(time.time(), ' : send success')
+#     while True:
+#         recv = client.recv(1024)
+#         if not recv:
+#             break
+#         msg += recv
+#
+# except:
+#     traceback.print_exc()
 
-except:
-    traceback.print_exc()
+msg = ""
